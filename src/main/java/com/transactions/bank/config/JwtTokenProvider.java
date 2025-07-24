80933575a9883157e3f3e6ba987766f2874714b0
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-07-24 13:53:04 -0500

    feat(auth): JwtTokenProvider para autenticación con JWT, expiracion and secret key

---
+++ src/main/java/com/transactions/bank/config/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package com.transactions.bank.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration}")
+    private int jwtExpirationInMs;
+
+    public String generarToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date fechaActual = new Date();
+        Date fechaExpiracion = new Date(fechaActual.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(fechaActual)
+                .setExpiration(fechaExpiracion)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String obtenerUsernameDelJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+}
