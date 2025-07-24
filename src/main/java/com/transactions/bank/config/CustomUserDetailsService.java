d852733077cc6772318d5c558248a625542bc049
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-07-24 15:14:11 -0500

    featfeat(auth): agrega servicio de UserDetails y repositorio para entidad Usuario

---
+++ src/main/java/com/transactions/bank/config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.transactions.bank.config;
+
+import com.transactions.bank.adapters.persistence.UsuarioRepository;
+import com.transactions.bank.domain.Usuario;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+    }
+}
