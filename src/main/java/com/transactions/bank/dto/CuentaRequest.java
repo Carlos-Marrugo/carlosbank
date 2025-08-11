796b18c3d2285f28d3c1a05b578417fb9ab5712b
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-08-11 17:43:20 -0500

    feat: servicio cuenta con crearCuenta con dto request y conversion a Response

---
+++ src/main/java/com/transactions/bank/dto/CuentaRequest.java
@@ -0,0 +1,42 @@
+package com.transactions.bank.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+
+public class CuentaRequest {
+    @NotBlank(message = "El número de cuenta es obligatorio")
+    private String numeroCuenta;
+
+    @NotBlank(message = "El nombre del propietario es obligatorio")
+    private String propietario;
+
+    @DecimalMin(value = "0.0", message = "El saldo inicial no puede ser negativo")
+    private BigDecimal saldo;
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/com/transactions/bank/services/CuentaService.java
@@ -0,0 +1,52 @@
+package com.transactions.bank.services;
+
+import com.transactions.bank.adapters.persistence.CuentaRepository;
+import com.transactions.bank.domain.Cuenta;
+import com.transactions.bank.domain.exceptions.CuentaExisteException;
+import com.transactions.bank.dto.CuentaRequest;
+import com.transactions.bank.dto.CuentaResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CuentaService {
+
+    @Autowired
+    private CuentaRepository cuentaRepository;
+
+    public CuentaService(CuentaRepository cuentaRepository) {
+        this.cuentaRepository = cuentaRepository;
+    }
+
+    private CuentaResponse convertirAResponse(Cuenta cuenta) {
+        return new CuentaResponse(
+                cuenta.getId(),
+                cuenta.getNumeroCuenta(),
+                cuenta.getPropietario(),
+                cuenta.getSaldo(),
+                cuenta.getFechaCreacion()
+        );
+    }
+
+    @Transactional
+    public CuentaResponse crearCuenta(CuentaRequest request) {
+        if(cuentaRepository.findByNumeroCuenta(request.getNumeroCuenta())!=null){
+            throw new CuentaExisteException("Esta cuenta ya esta registrada!");
+        }
+
+        Cuenta cuenta = Cuenta.builder()
+                .numeroCuenta(request.getNumeroCuenta())
+                .propietario(request.getPropietario())
+                .saldo(request.getSaldo())
+                .build();
+
+        cuentaRepository.save(cuenta);
+
+        return convertirAResponse(cuenta);
+    }
+
+
+
+}
